74f23ab3578710ae399f8a334437b751f0a80f8f
oirase <cashewnutmuffin@outlook.com>
2020-09-11 03:55:25 +0900

    all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ interface Props {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
   const res = await fetch('https://api.github.com/repos/zeit/next.js')
   const json = await res.json()
 
@@ -16,33 +15,35 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     props: {
       server: context.params,
       stars: json.stargazers_count,
-    }
+    },
   }
 }
 
 //const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 const App = ({ server, stars }: Props) => {
-/*
+  /*
   const sendData = async() = {
     await fetch()
   }
 */
-  const { data, error } = useSWR('https://nextjs-blog-ssr.herokuapp.com/api/hello', fetch)
+  const { data, error } = useSWR(
+    'https://nextjs-blog-ssr.herokuapp.com/api/hello',
+    fetch
+  )
   if (error) return <div>faild to load</div>
   if (!data) return <div>loading...</div>
-    console.log(data)
+  console.log(data)
 
-    return (
-      <>
-        <h1>studySearch</h1>
-        <p>server: {server && server}</p>
-        <p>stars: {stars && stars}</p>
-        {/*<p>{data.name}!</p>*/}
-        {/*<p onClick={sendDtata}>send</p>*/}
-      </>
-      )
-  }
-
+  return (
+    <>
+      <h1>studySearch</h1>
+      <p>server: {server && server}</p>
+      <p>stars: {stars && stars}</p>
+      {/*<p>{data.name}!</p>*/}
+      {/*<p onClick={sendDtata}>send</p>*/}
+    </>
+  )
+}
 
 export default App
