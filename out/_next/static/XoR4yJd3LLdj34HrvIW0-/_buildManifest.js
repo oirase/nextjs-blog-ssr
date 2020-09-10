e3a7cdb335c696b1db9ea17e38843b1df1975c24
oirase <cashewnutmuffin@outlook.com>
2020-09-10 14:10:00 +0900

    all

---
+++ out/_next/static/XoR4yJd3LLdj34HrvIW0-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002Fa2ff7a21d4526a66bd8a.css","static\u002Fchunks\u002Fpages\u002Findex-064598e981b2c14fe66a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"],"/posts":["static\u002Fchunks\u002Fpages\u002Fposts-624811d160df2c29ec05.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-
 const App = () => {
   return <h1>studySearch</h1>
 }
