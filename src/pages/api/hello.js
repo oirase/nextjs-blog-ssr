d9c70e233cd6cf5fc11a49b5d01d70ebd4cb8647
oirase <cashewnutmuffin@outlook.com>
2020-09-11 03:33:35 +0900

    all

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,4 +1,7 @@
 export default (req, res) => {
-  res.status(200).json({ text: 'Hello' })
+  //res.status(200).json({ text: 'Hello' })
   //return {{ text: 'Hello '}}
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ name: 'John Doe' }))
 }
