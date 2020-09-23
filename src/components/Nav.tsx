94e9202ff73d6a4343c376db507e4bdbf01f8aee
oirase <cashewnutmuffin@outlook.com>
2020-09-24 02:06:54 +0900

    all

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import Tab from '~/components/Tab'
 
 const Nav = () => {
+  const router = useRouter()
+
   return (
     <nav>
       <Tab href="/">New</Tab>
       <Tab href="/category">Category</Tab>
       <Tab href="/search">Search</Tab>
-      <Tab href="/article">Article</Tab>
+      {router.pathname.includes('articles') ? (
+        <Tab href="/article">Article</Tab>
+      ) : null}
     </nav>
   )
 }
