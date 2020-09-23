6edb21e9d44544a45d43618b170812d4a862126a
oirase <cashewnutmuffin@outlook.com>
2020-09-23 15:33:49 +0900

    all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
 import Layout from '~/components/Layout'
+import { getSortedPostsData } from '../lib/posts'
 
-const Index = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+const Index = ({ allPostsData }) => {
   return (
     <Layout>
       <p>New Page</p>
+      <ul>
+        {allPostsData.map(({ id, date, title }) => (
+          <li key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
