710a253bc7e7bd7555c5532de66f40129258084b
oirase <cashewnutmuffin@outlook.com>
2020-09-23 23:27:38 +0900

    all

--- src/pages/api/hello.tsx
+++
@@ -1,7 +0,0 @@
-export default (req, res) => {
-  //res.status(200).json({ text: 'Hello' })
-  //return {{ text: 'Hello '}}
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ name: 'John Doe' }))
-}
