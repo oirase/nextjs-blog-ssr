b893efcf45466aa2cfb8ed3e34691c616d252ed7
oirase <cashewnutmuffin@outlook.com>
2020-09-27 14:38:52 +0900

    all

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,12 +1,27 @@
 import Link from 'next/link'
 import Layout from '~/components/Layout'
 import { getPostData } from '~/lib/posts'
+import { useRef } from 'react'
 
 const Search = () => {
+  const inputSearch = useRef()
+
+  const handleSearch = (event) => {
+    fetch('api/search')
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result)
+      })
+    event.preventDefault()
+  }
 
   return (
     <Layout>
       <p>search page</p>
+      <form onSubmit={(e) => handleSearch(e)}>
+        <input type="text" ref={inputSearch} />
+        <button type="submit">検索</button>
+      </form>
     </Layout>
   )
 }
