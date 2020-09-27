3008553bac8be7f7991f91924a8b672cb9effa2d
oirase <cashewnutmuffin@outlook.com>
2020-09-27 20:06:47 +0900

    all

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -3,22 +3,23 @@ import redis from 'redis'
 export default async function getSearch(req, res) {
   const client = redis.createClient(process.env.REDIS_URL)
   client.on('error', function (err) {
-    throw err
-  })
+  throw err
+})
 
   const body = req.body
   console.log(req.body)
   let result = {}
-  client.hgetall('frameworks', function (err, value) {
+  client.hgetall("frameworks", function(err, value) {
     result = value
     client.quit()
     res.json(result)
   })
 
-  /*
+
+/*
   res.json({
     name: "apple"
   })
 */
-  res.json(result)
+  //res.json(result)
 }
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -4,19 +4,22 @@ import { getPostData } from '~/lib/posts'
 import { useRef } from 'react'
 
 const Search = () => {
+
   const inputSearch = useRef()
 
   const handleSearch = (event) => {
     const requestOptions = {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name: 'melon' }),
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ name: 'melon' })
     }
     fetch('api/search', requestOptions)
       .then((res) => res.json())
-      .then((result) => {
-        console.log(result)
-      })
+      .then(
+        (result) => {
+          console.log(result)
+        }
+      )
     event.preventDefault()
   }
 
@@ -24,8 +27,11 @@ const Search = () => {
     <Layout>
       <p>search page</p>
       <form onSubmit={(e) => handleSearch(e)}>
-        <input type="text" ref={inputSearch} />
-        <button type="submit">検索</button>
+        <input
+          type="text"
+          ref={inputSearch}
+        />
+      <button type="submit">検索</button>
       </form>
     </Layout>
   )
