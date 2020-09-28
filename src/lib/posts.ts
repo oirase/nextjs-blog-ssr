43335720d85e28e8f6c8e32c58c2edfcf385db19
oirase <cashewnutmuffin@outlook.com>
2020-09-28 10:00:47 +0900

    all

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -7,9 +7,10 @@ import html from 'remark-html'
 const postsDirectory = path.join(process.cwd(), 'src/posts')
 
 export const getSortedPostsData = () => {
+
   const fileNames = fs.readdirSync(postsDirectory)
 
-  const allPostsData = fileNames.map((fileName) => {
+  const allPostsData = fileNames.map(fileName => {
     const id = fileName.replace(/\.md$/, '')
     const fullPath = path.join(postsDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
@@ -17,11 +18,7 @@ export const getSortedPostsData = () => {
 
     return {
       id,
-      ...(matterResult.data as {
-        date: string
-        title: string
-        category: string
-      }),
+      ...(matterResult.data as { date: string; title: string; category: string;})
     }
   })
   return allPostsData.sort((a, b) => {
@@ -36,11 +33,11 @@ export const getSortedPostsData = () => {
 export const getAllPostIds = () => {
   const fileNames = fs.readdirSync(postsDirectory)
 
-  return fileNames.map((fileName) => {
+  return fileNames.map(fileName => {
     return {
       params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
+        id: fileName.replace(/\.md$/, '')
+      }
     }
   })
 }
@@ -59,14 +56,14 @@ export const getPostData = async (id) => {
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...matterResult.data
   }
 }
 
 export const getAllPostsData = () => {
   const fileNames = fs.readdirSync(postsDirectory)
 
-  const allPostsData = fileNames.map((fileName) => {
+  const allPostsData = fileNames.map(fileName => {
     const id = fileName.replace(/\.md$/, '')
     const fullPath = path.join(postsDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
@@ -74,12 +71,8 @@ export const getAllPostsData = () => {
 
     return {
       id,
-      ...(matterResult.data as {
-        date: string
-        title: string
-        category: string
-      }),
-      content: matterResult.content,
+      ...(matterResult.data as { date: string; title: string; category: string;}),
+      content: matterResult.content
     }
   })
 
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,58 +3,60 @@ import Layout from '~/components/Layout'
 import { getAllPostsData } from '~/lib/posts'
 import { useRef, useState } from 'react'
 
-export function getStaticProps() {
+export function getStaticProps () {
   const allPostsData = getAllPostsData()
   return {
     props: {
-      allPostsData,
-    },
+      allPostsData
+    }
   }
 }
 
 const Search = ({ allPostsData }) => {
+
   const inputSearch = useRef(null)
   const [searchResult, setSearchResult] = useState([])
 
   const handleSearch = () => {
     const searchWord = inputSearch.current.value
-    const result = allPostsData.find((data) => {
-      for (const key of Object.keys(data)) {
+    const result = allPostsData.find(data=>{
+      for (let key of Object.keys(data)) {
         console.log(key)
-        console.log(typeof data[key])
+        console.log( typeof data[key])
         const value = data[key]
-        if (value.includes(searchWord)) {
+        if(value.includes(searchWord)) {
           return true
         }
       }
     })
     console.log(result)
-    setSearchResult(result)
+    //setSearchResult(result)
   }
 
   return (
     <Layout>
       <p>search page</p>
-      <input type="text" ref={inputSearch} />
+        <input
+          type="text"
+          ref={inputSearch}
+        />
       <button onClick={handleSearch}>検索</button>
       <ul>
-        {searchResult.length ? (
+        {searchResult.length ?
           searchResult.map(({ id, date, title, category }) => (
-            <li key={id}>
-              <Link href={`/article/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              {id}
-              <br />
-              {date}
-              <br />
-              {category}
-            </li>
-          ))
-        ) : (
-          <li>none</li>
-        )}
+          <li key={id}>
+          <Link href={`/article/${id}`}>
+            <a>{title}</a>
+          </Link>
+            <br />
+            {id}
+            <br />
+            {date}
+            <br />
+            {category}
+          </li>
+        )) :
+        <li>none</li>}
       </ul>
     </Layout>
   )
