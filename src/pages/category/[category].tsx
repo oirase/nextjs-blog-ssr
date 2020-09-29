c8882fc962e660801707feb3cb06c47e91121b00
oirase <cashewnutmuffin@outlook.com>
2020-09-30 02:52:20 +0900

    all

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -1,37 +1,36 @@
 import Link from 'next/link'
+import { useState } from 'react'
 import Layout from '~/components/Layout'
 import { getSortedPostsData } from '~/lib/posts'
 import { PostMetaType } from '~/types/post'
 import ArticleItem from '~/components/ArticleItem'
+import Paginate from '~/components/Paginate'
+import ListRender from '~/components/ListRender'
 
 export async function getStaticPaths() {
   const allPostsData = getSortedPostsData()
-  let allPostsCategory = allPostsData.map(
-    ({ category }: PostMetaType) => category
-  )
+  let allPostsCategory = allPostsData.map(({ category }: PostMetaType) => category)
   allPostsCategory = Array.from(new Set(allPostsCategory))
-  const paths = allPostsCategory.map((category) => {
+  const paths = allPostsCategory.map(category => {
     return {
       params: {
-        category,
-      },
+        category
+      }
     }
   })
   return {
     paths,
-    fallback: false,
+    fallback: false
   }
 }
 
 export async function getStaticProps({ params }) {
   const allPostsData = getSortedPostsData()
-  const categoryPostsData = allPostsData.filter(
-    ({ category }) => category === params.category
-  )
+  const categoryPostsData = allPostsData.filter(({ category }) => category === params.category)
   return {
     props: {
-      categoryPostsData,
-    },
+      categoryPostsData
+    }
   }
 }
 
@@ -40,14 +39,25 @@ type Props = {
 }
 
 const Category = ({ categoryPostsData }: Props) => {
+
+  const [offset, setOffset] = useState(1)
+
   return (
     <Layout>
       <p>category page</p>
-      <ul>
-        {categoryPostsData.map(({ id, ...rest }) => (
-          <ArticleItem key={id} id={id} {...rest} />
-        ))}
-      </ul>
+      <Paginate
+              offset={offset}
+              length={categoryPostsData.length}
+              setOffset={setOffset}
+            />
+      <ListRender
+        data={categoryPostsData}
+        offset={offset}
+        render={
+          (data)=>
+            <ArticleItem {...data} />
+        }
+      />
     </Layout>
   )
 }
