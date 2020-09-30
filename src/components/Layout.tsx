4dc7cc41be92a380940c63eeb51775bdb4e71583
oirase <cashewnutmuffin@outlook.com>
2020-09-30 11:56:22 +0900

    all

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import Meta from '~/components/Meta'
 import Header from '~/components/Header'
 import Nav from '~/components/Nav'
 import Footer from '~/components/Footer'
+import styles from '~/components/layout.module.css'
 
 const Layout: FC = ({ children }) => (
   <>
