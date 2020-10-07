7f57fe4438fc806990a1250d5f92b0c8d83144ac
oirase <cashewnutmuffin@outlook.com>
2020-10-08 02:56:27 +0900

    all

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,5 +1,11 @@
 import { FC } from 'react'
 import Link from 'next/link'
+import { white,
+         green,
+         orange,
+         red,
+         blue
+      } from '~/styles/variables'
 
 type Props = {
   href: string
@@ -8,8 +14,54 @@ type Props = {
 const Tab: FC<Props> = ({ href, children}) => {
   return (
   <Link href={href}>
-    <button>
+    <button className="tab">
       {children}
+          <style jsx>{`
+
+      $white: #FFF;
+      $green: #33CC99;
+      $orange: #FF9900;
+      $red: #FF3300;
+      $blue: #00CCCC;
+
+      .tab {
+        padding: 2rem 0;
+        text-align: center;
+        font-size: 1.4rem;
+        width: 20rem;
+        max-width: 24%;
+        margin-right: 0.8%;
+        border: 1px solid $white;
+        border-bottom: none;
+        background: $blue;
+        border-radius: 10px 10px 0 0;
+
+
+        &:nth-child(1) {
+          background: $green;
+          border-color: lighten($green, 40%);
+        }
+
+        &:nth-child(2) {
+          background: $orange;
+          border-color: lighten($orange, 40%);
+        }
+
+        &:nth-child(3) {
+          background: $red;
+          border-color: lighten($red, 40%);
+        }
+
+        &:nth-child(4) {
+          background: $blue;
+          border-color: lighten($blue, 40%);
+        }
+
+        &:last-child {
+          margin-right: 0;
+        }
+      }
+    `}</style>
     </button>
   </Link>
   )
