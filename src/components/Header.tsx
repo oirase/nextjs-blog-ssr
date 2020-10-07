4549093a1009cb3edb34bbb1dc58b178ef7cc20c
oirase <cashewnutmuffin@outlook.com>
2020-10-08 04:09:12 +0900

    all

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,25 @@
+import { fontBase,
+         white
+        } from '~/styles/variables'
 
 const Header = () => (
- <header>
-  <h1>WebCreateBlog</h1>
+ <header className="header">
+  <h1 className="header__h1">WebCreateBlog</h1>
+  <style jsx>{`
+    .header {
+      height: 16rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: ${white};
+
+      &__h1 {
+        font-family: 'Abril Fatface', ${fontBase};
+        font-size: 4rem;
+        font-weight: normal;
+      }
+    }
+  `}</style>
  </header>
 )
 
