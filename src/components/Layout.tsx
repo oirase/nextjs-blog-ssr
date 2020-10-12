ecc8325d8992325adc0e6fdaf8d0ae4813a6341d
oirase <cashewnutmuffin@outlook.com>
2020-10-13 03:36:44 +0900

    all

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -27,7 +27,7 @@ const Layout: FC = ({ children }) => (
         }
 
         .top-view {
-          background-image: url('/images/layout/image06.jpg');
+          background-image: url('/images/layout/image01.jpg');
           background-color: purple;
           border-radius: 10px 10px 0 0;
         }
