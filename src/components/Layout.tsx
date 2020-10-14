e2810a9d12c197edc6f5088e9ca4c69f72af5216
oirase <cashewnutmuffin@outlook.com>
2020-10-14 20:06:05 +0900

    all100vh

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -38,7 +38,7 @@ const Layout: FC = ({ children }) => (
         .contents {
           background: ${yellow};
           padding: 2rem 0;
-          min-height: 100vh;
+          min-height: 100%;
         }
       `}</style>
     </div>
