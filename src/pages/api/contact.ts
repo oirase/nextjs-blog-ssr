6887c51d3e940e0d9b18a948b8ea7af48f084779
oirase <cashewnutmuffin@outlook.com>
2020-10-18 05:36:45 +0900

    all

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,6 +1,6 @@
 
 export default async function getData(req, res) {
-  const { body } = req
-
-  res.status(200).json(body)
+  //const { body } = req
+  const test = req.body.test
+  res.status(200).json({sample: test})
 }
