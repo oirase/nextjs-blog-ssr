09d25c091b10fd7fbc3095dd16ed24c0c8e3d718
oirase <cashewnutmuffin@outlook.com>
2020-10-19 03:07:01 +0900

    all

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,23 +1,24 @@
 import { useState } from 'react'
+import Link from 'next/link'
 import useSWR from 'swr'
 import ContactForm from '~/components/ContactForm'
 import Layout from '~/components/Layout'
 
-
+type contactFormType = {
+  name: string
+  email: string
+  subject: string
+  body: string
+}
 
 const Contact = () => {
 
-  const [state, setState] = useState(null)
+  const [state, setState] = useState<contactFormType | null>(null)
   const { data, error } = useSWR(state ? '/api/contact' : null, ()=> fetcher(state), {
     revalidateOnFocus: false
   })
 
-  const fetcher =  async (data: any) => {
-    /*
-    const data = {
-      test: 'swr-test'
-    }
-    */
+  const fetcher =  async (data: contactFormType) => {
     const url = '/api/contact'
     //const data = { test: "api-test" }
     const res = await fetch(url, {
@@ -33,45 +34,35 @@ const Contact = () => {
     return json
   }
 
-
-  const setData = async (data) => {
-    /*
-    const url = '/api/contact'
-    //const data = { test: "api-test" }
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    const json = await res.json()
-    console.log(json)
-    setState(true)
-    */
-  }
-
-  const handleClick = (data) => {
+  const handleClick = (data: contactFormType) => {
     setState(data)
   }
 
-  const complete =
-    <div>
-      <p>送信が完了しました</p>
-    </div>
+  const complete = <>
+        <p>送信が完了しました</p>
+        <Link　href="/">
+          <a>トップページ</a>
+        </Link>
+      </>
 
-  const loading =
-    <div>
-      <p>送信中...</p>
-    </div>
+  const loading = <p>送信中...</p>
 
   return (
     <Layout>
+      <div className="result-info">
       { error && <p>error</p>}
+      </div>
       { !state ? null
                : data ? complete
                       : loading }
       { !data ? <ContactForm onSubmit={handleClick} /> : null }
+      <style jsx>{`
+        .result-info {
+          text-align: center;
+          padding: 2rem 0;
+          line-height: 2;
+        }
+      `}</style>
     </Layout>
   )
 }
